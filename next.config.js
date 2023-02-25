a7189aa71b7029a250683dd8a11a3e7eb8a8567f
x8080808080 <109244972+x8080808080@users.noreply.github.com>
2023-02-25 12:08:51 +0800

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@ module.exports = {
 	async headers() {
 		return [
 			{
-				source: '/(.*)',
+				source: '/',
 				headers: [
 					{
 						key: 'Access-Control-Allow-Origin',
-						value: '*'
+						value: 'https://sweets-g-bakeshop.vercel.app'
 					}
 				]
 			}
