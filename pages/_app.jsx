da6c7668c2c513674193953c2c701c8ff0545126
Tristan Felizarta <coc.tanfelizarta@gmail.com>
2023-02-26 13:38:12 +0800

    fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import { SessionProvider } from 'next-auth/react'
 import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import AppProvider from 'components/_app'
-import { MessengerChat } from 'react-messenger-chat-plugin'
 
 const App = ({ Component, pageProps: { session, ...pageProps } }) => {
 	const [queryClient] = useState(() => new QueryClient())
 	const layout = Component.layout || ((page) => page)
+
+	useEffect(() => {
+		if (typeof window !== 'undefined') {
+			window.fbAsyncInit = function () {
+				FB.init({
+					xfbml: true,
+					version: 'v10.0'
+				})
+			}
+		}
+	}, [])
 
 	return (
 		<>
@@ -18,7 +28,7 @@ const App = ({ Component, pageProps: { session, ...pageProps } }) => {
 				<link rel="icon" type="image/png" size="96x96" href="favicon.svg" />
 			</Head>
 
-			<MessengerChat pageId="100090256728980" />
+			<div className="fb-customerchat" attribution="biz_inbox" page_id="100090256728980" />
 
 			<SessionProvider session={session}>
 				<QueryClientProvider client={queryClient}>
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,6 +8,26 @@ export default class Document extends NextDocument {
 			<Html lang="en">
 				<Head>
 					<link rel="preload" href="/fonts/Inter.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v10.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    `
+						}}
+					/>
 				</Head>
 
 				<body>
